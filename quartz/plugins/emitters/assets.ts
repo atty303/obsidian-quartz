0c021311729698d6026b7e967d23c0134935049a
Koji AGAWA <atty303@gmail.com>
2025-06-30 23:13:09 +0900

    fix(assets): include articles in the glob pattern for files to copy

--- quartz/plugins/emitters/assets.ts
+++ quartz/plugins/emitters/assets.ts
@@ -8,7 +8,7 @@ import { QuartzConfig } from "../../cfg"
 
 const filesToCopy = async (argv: Argv, cfg: QuartzConfig) => {
   // glob all non MD files in content folder and copy it over
-  return await glob(["publish/**", "zettelkasten/**"], argv.directory, ["**/*.md", ...cfg.configuration.ignorePatterns])
+  return await glob(["articles/**", "publish/**", "zettelkasten/**"], argv.directory, ["**/*.md", ...cfg.configuration.ignorePatterns])
 }
 
 const copyFile = async (argv: Argv, fp: FilePath) => {
