10e3c85a2823a701984f43e5079ae8868b519375
Koji AGAWA <atty303@gmail.com>
2025-07-11 20:06:56 +0900

    fix(ogImage): update font settings to use Japanese typography

--- quartz/plugins/emitters/ogImage.tsx
+++ quartz/plugins/emitters/ogImage.tsx
@@ -111,8 +111,8 @@ export const CustomOgImages: QuartzEmitterPlugin<Partial<SocialImageOptions>> = 
     },
     async *emit(ctx, content, _resources) {
       const cfg = ctx.cfg.configuration
-      const headerFont = cfg.theme.typography.header
-      const bodyFont = cfg.theme.typography.body
+      const headerFont = cfg.theme.typography.japanese
+      const bodyFont = cfg.theme.typography.japanese
       const fonts = await getSatoriFonts(headerFont, bodyFont)
 
       for (const [_tree, vfile] of content) {
@@ -122,8 +122,8 @@ export const CustomOgImages: QuartzEmitterPlugin<Partial<SocialImageOptions>> = 
     },
     async *partialEmit(ctx, _content, _resources, changeEvents) {
       const cfg = ctx.cfg.configuration
-      const headerFont = cfg.theme.typography.header
-      const bodyFont = cfg.theme.typography.body
+      const headerFont = cfg.theme.typography.japanese
+      const bodyFont = cfg.theme.typography.japanese
       const fonts = await getSatoriFonts(headerFont, bodyFont)
 
       // find all slugs that changed or were added
